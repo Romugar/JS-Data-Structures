02f9340d579e19748f8975e66b1ea21695b16a0a
romugar <roberto2925@hotmail.com>
2018-10-02 13:43:37 +0200

    added hash table class

--- DataStructures.js
+++ DataStructures.js
@@ -196,77 +196,192 @@ function Set() {
   var collection = [];
   // this method will check for the presence of an element and return true or false
   this.has = function(element) {
-      return (collection.indexOf(element) !== -1);
+    return collection.indexOf(element) !== -1;
   };
   // this method will return all the values in the set
   this.values = function() {
-      return collection;
+    return collection;
   };
   // this method will add an element to the set
   this.add = function(element) {
-      if(!this.has(element)){
-          collection.push(element);
-          return true;
-      }
-      return false;
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
   };
   // this method will remove an element from a set
   this.remove = function(element) {
-      if(this.has(element)){
-         var index = collection.indexOf(element);
-          collection.splice(index,1);
-          return true;
-      }
-      return false;
+    if (this.has(element)) {
+      var index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
   };
   // this method will return the size of the collection
   this.size = function() {
-      return collection.length;
+    return collection.length;
   };
   // this method will return the union of two sets
   this.union = function(otherSet) {
-      var unionSet = new Set();
-      var firstSet = this.values();
-      var secondSet = otherSet.values();
-      firstSet.forEach(function(e){
-          unionSet.add(e);
-      });
-      secondSet.forEach(function(e){
-          unionSet.add(e);
-      });
-      return unionSet;
+    var unionSet = new Set();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function(e) {
+      unionSet.add(e);
+    });
+    secondSet.forEach(function(e) {
+      unionSet.add(e);
+    });
+    return unionSet;
   };
   // this method will return the intersection of two sets as a new set
   this.intersection = function(otherSet) {
-      var intersectionSet = new Set();
-      var firstSet = this.values();
-      firstSet.forEach(function(e){
-          if(otherSet.has(e)){
-              intersectionSet.add(e);
-          }
-      });
-      return intersectionSet;
+    var intersectionSet = new Set();
+    var firstSet = this.values();
+    firstSet.forEach(function(e) {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+    });
+    return intersectionSet;
   };
   // this method will return the difference of two sets as a new set
   this.difference = function(otherSet) {
-      var differenceSet = new Set();
-      var firstSet = this.values();
-      firstSet.forEach(function(e){
-          if(!otherSet.has(e)){
-              differenceSet.add(e);
-          }
-      });
-      return differenceSet;
+    var differenceSet = new Set();
+    var firstSet = this.values();
+    firstSet.forEach(function(e) {
+      if (!otherSet.has(e)) {
+        differenceSet.add(e);
+      }
+    });
+    return differenceSet;
   };
   // this method will return true if the otherSet is contained in the set
   this.subset = function(otherSet) {
-      var isContained = true;
-      var firstSet = this.values();
-      firstSet.forEach((d) => {
-          if (!otherSet.has(d)) {
-              isContained = false;
-          }
-      });
-      return isContained;
-  }  
+    var isContained = true;
+    var firstSet = this.values();
+    firstSet.forEach(d => {
+      if (!otherSet.has(d)) {
+        isContained = false;
+      }
+    });
+    return isContained;
+  };
 }
+
+//--------------------------//
+// MAP DATA STRUCTURE CLASS //
+//--------------------------//
+//  Maps are data structures that store key-value pairs. This class is like the built-in map in ES6
+
+var Map = function() {
+  this.collection = {};
+  // accepts a key, value pair to add to the map.
+  this.add = function(key, value) {
+    this.collection[key] = value;
+  };
+  // accepts a key and removes the associated key, value pair
+  this.remove = function(key) {
+    delete this.collection[key];
+  };
+  // accepts a key and returns the stored value
+  this.get = function(key) {
+    return this.collection[key];
+  };
+  // accepts a key and returns true if the key exists or false if it doesn't.
+  this.has = function(key) {
+    return this.collection.hasOwnProperty(key);
+  };
+  // returns an array of all the values in the map
+  this.values = function() {
+    return Object.values(this.collection);
+  };
+  // returns the number of items in the map
+  this.size = function() {
+    return Object.keys(this.collection).length;
+  };
+  //empties the map
+  this.clear = function() {
+    this.collection = {};
+  };
+};
+
+//------------//
+// HASH TABLE //
+//------------//
+// A Hash table is used to implement associative arrays.
+// The way a hash table works is that it takes a key input and hashes this key in a deterministic way to some numerical value.
+// This numerical value is then used as the actual key the associated value is stored by.
+// Then, if you try to access the same key again, the hashing function will process the key,
+// return the same numerical result, which will then be used to look up the associated value. This provides very efficient O(n) lookup time on average.
+
+// funcion que hashea la key convirtiendo cada letra en su valor numerico y sumandolos
+var called = 0;
+var hash = string => {
+  called++;
+  var hash = 0;
+  for (var i = 0; i < string.length; i++) {
+    hash += string.charCodeAt(i);
+  }
+  return hash;
+};
+// Clase para gestionar la Hash Table
+let HashTable = function() {
+  this.collection = {};
+  // Metodo que añade key-value
+  this.add = function(key, value) {
+    // hashea el key
+    let hashed = hash(key);
+    // si hay algun hash igual lo itera, si algun key es igual a la añadida la sobreescribe, sino añade otro [key: value] en ese hash
+    if (this.collection.hasOwnProperty(hashed)) {
+      let bucket = this.collection[hashed].filter(
+        item => !item.hasOwnProperty(key)
+      );
+      bucket.push({ [key]: value });
+      this.collection[hashed] = bucket;
+      // si el hash es nuevo lo crea y añade [key:value]
+    } else {
+      this.collection[hashed] = [{ [key]: value }];
+    }
+  };
+  // metodo que borra [key: value]
+  this.remove = function(key) {
+    let hashed = hash(key);
+    // si hay un hash igual y tiene solo un valor lo borra
+    if (this.collection.hasOwnProperty(hashed)) {
+      if (this.collection[hashed].length == 1) {
+        delete this.collection[hashed];
+        // sino, lo itera y busca una key igual y la borra
+      } else {
+        this.collection[hashed] = this.collection[hashed].filter(
+          item => !item.hasOwnProperty(key)
+        );
+      }
+      // sino encuentra nada devuelve null
+    } else {
+      return null;
+    }
+  };
+  // metodo para burcar el valor de una key
+  this.lookup = function(key) {
+    let hashed = hash(key);
+    // si tiene un hash igual, lo itera (recursivamente) y busca un key igual y devuelve su value
+    if (this.collection.hasOwnProperty(hashed)) {
+      let bucket = this.collection[hashed];
+      function find(index) {
+        if (bucket[index].hasOwnProperty(key)) {
+          return bucket[index][key];
+        } else if (index != 0) {
+          return find(index - 1);
+        } else {
+          return null;
+        }
+      }
+      return find(bucket.length - 1);
+    } else {
+      return null;
+    }
+  };
+};
--- README.md
+++ README.md
@@ -19,3 +19,18 @@ Clase Queue que escribe hasta el final de la lista y luego vuelve desde el princ
 - THE SET
 
 A Set is like an array, but it cannot contain duplicate values. The typical use for a Set is to simply check for the presence of an item. It has union, intersection, difference and subset methods.
+
+- MAP DATA STRUCTURE
+
+Maps are data structures that store key-value pairs.This class is like the built-in map in ES6. Include this methods: 
+1. **add** accepts a key, value pair to add to the map.
+2. **remove** accepts a key and removes the associated key, value pair.
+3. **get** accepts a key and returns the stored value.
+4. **has** accepts a key and returns true if the key exists or false if it doesn't.
+5. **values** returns an array of all the values in the map.
+6. **size** returns the number of items in the map.
+7. **clear** empties the map.
+
+- HASH TABLE
+
+A Hash table is used to implement associative arrays. The way a hash table works is that it takes a key input and hashes this key in a deterministic way to some numerical value. This numerical value is then used as the actual key the associated value is stored by. Then, if you try to access the same key again, the hashing function will process the key, return the same numerical result, which will then be used to look up the associated value. This provides very efficient O(n) lookup time on average. It has add, remove and lookup methods.
